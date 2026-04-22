eb9e5aeeb9a95e9f175716f840a8cd8c322eb457
Harry-Rudd <hprudd42@tntech.edu>
2026-04-21 20:28:00 -0500

    Added 50021L Cable to cblArr
    Started an array for cables and added the 50021L cable characteristics from the spec sheet to the first cable.

--- Software/Cable_Simulations/Cable_Reflections.py
+++ Software/Cable_Simulations/Cable_Reflections.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define constants found in Siemens sheet
-CLOCK_FREQUENCY = 2.5 * np.power(10,6)
+CLOCK_FREQUENCY = 2.5*np.power(10,6)
 DC_VOLTAGE = 48
 MAX_RIPPLE = 0.2
 DC_POWER = 100
@@ -25,3 +25,4 @@ class Cable():
         self.capacitanceCS - capacitanceCS # Conductor to shield capacitance per meter
         self.LRRatio = LRRatio # Inductance to resistance ratio
 
+cblArr = np.array([Cable('50021L', 5.2*np.power(10,-9), 120, 36.7*np.power(10,-3), np.power(10,-6), 46*np.power(10,-9), 82*np.power(10,-9), 25*np.power(10,-6))])
