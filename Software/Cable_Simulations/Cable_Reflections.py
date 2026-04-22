f7f3c3bfa9bbf00cdb09bbeda2e31242e45ff06b
Harry-Rudd <hprudd42@tntech.edu>
2026-04-21 20:06:23 -0500

    Added Rest of Datasheet Info to Cable Class
    Added the rest of the parameters needed from the datasheet to the cable class

--- Software/Cable_Simulations/Cable_Reflections.py
+++ Software/Cable_Simulations/Cable_Reflections.py
@@ -7,8 +7,14 @@ import matplotlib.pyplot as plt
 
 # Define cable class so multiple cables can be tested
 class Cable():
-    def __init__(self, name, delay, ):
+    def __init__(self, name, delay, characteristicImpedance, DCR, inductance, capacitanceCC, capacitanceCS, LRRatio):
         # All numerical values are in base SI units
         self.name = name
-        self.delay = delay # Delay found in datasheet
-        self.propagationSpeed = 1/delay # Propagation speed needed to find wavelength
+        self.delay = delay # Delay found in datasheet [seconds per meter]
+        self.propagationSpeed = 1/delay # Propagation speed needed to find wavelength [meters per second]
+        self.characteristicImpedance = characteristicImpedance
+        self.DCR = DCR # Direct Current Resistance (DCR) is the resistance at 0 Hz
+        self.inductance = inductance # Inductance per meter
+        self.capacitanceCC - capacitanceCC # Conductor to conductor capacitance per meter
+        self.capacitanceCS - capacitanceCS # Conductor to shield capacitance per meter
+        self.LRRatio = LRRatio # Inductance to resistance ratio
