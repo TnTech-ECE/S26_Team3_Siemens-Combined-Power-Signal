51620faef710eb651e3b50a403a74b2bc1a82e56
Harry-Rudd <hprudd42@tntech.edu>
2026-04-21 19:18:49 -0500

    Started Cable Class
    Started a cable class definition so we can easily test multiple different cables using information from the datasheet

--- Software/Cable_Simulations/Cable_Reflections.py
+++ Software/Cable_Simulations/Cable_Reflections.py
@@ -1,5 +1,12 @@
 # Author: Harry Rudd
 # Date Started: 4/21/26
 
+
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Define cable class so multiple cables can be tested
+class cable():
+    def __init__(self, name, delay):
+        self.name = name
+        self.delay = delay
