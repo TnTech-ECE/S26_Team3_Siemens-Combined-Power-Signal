02ae4df65f8ff8df553523f7998ae345e58da2f6
Harry-Rudd <hprudd42@tntech.edu>
2026-04-21 19:27:58 -0500

    Added comments and words to dictionary
    Added comments and the words microcontroller and datasheet to the dictionary. I also continued developing cable class

--- Software/Cable_Simulations/Cable_Reflections.py
+++ Software/Cable_Simulations/Cable_Reflections.py
@@ -1,12 +1,14 @@
 # Author: Harry Rudd
 # Date Started: 4/21/26
 
-
+# Add dependencies
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Define cable class so multiple cables can be tested
-class cable():
-    def __init__(self, name, delay):
+class Cable():
+    def __init__(self, name, delay, ):
+        # All numerical values are in base SI units
         self.name = name
-        self.delay = delay
+        self.delay = delay # Delay found in datasheet
+        self.propagationSpeed = 1/delay # Propagation speed needed to find wavelength
